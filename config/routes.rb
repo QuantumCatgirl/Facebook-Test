9ff22e4f6fb28a2688be1960f21c25d1f851d43b
WillT <will.tomlins@unboxedconsulting.com>
2010-06-18 14:27:09 +0100

    some tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.connect '/facebook/application_tab', :controller => :test, :action => :test
+  
   map.connect '/', :controller => :test, :action => :test
   map.connect '/:path', :controller => :test, :action => :test
 end
